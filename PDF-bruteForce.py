94727935bc5459548982f1517e4bec2c7fc8dec4
Emre İşlek <95578438+islek-emre@users.noreply.github.com>
2022-01-07 19:25:56 +0300

    Update PDF-bruteForce.py

--- PDF-bruteForce.py
+++ PDF-bruteForce.py
@@ -1,5 +1,4 @@
 #pip3 install pikepdf tqdm
-
 import pikepdf
 from tqdm import tqdm
 
@@ -11,7 +10,7 @@ print ("Change the name of your wordlist file to wordlist. You can work with lar
 passwords = [ line.strip() for line in open("wordlist.txt") ]
 for password in tqdm(passwords, "Decrypting PDF"):
     try:
-        # PDF file
+        # open PDF file
         with pikepdf.open("locked.pdf", password=password) as pdf:
             # Password decrypted successfully, break out of the loop
             print("[+] Password found:", password)
